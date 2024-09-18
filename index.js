6af21595ed7f28b64919dd1f6e8116e5b0f5857a
sanghp <haphuocsang1801@gmail.com>
2024-09-18 19:34:36 +0700

    docker compose

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 const port = process.env.PORT || 3001
 app.get("/", (req, res) => {
-  res.send("<h1>Hello World!!</h1>")
+  res.send("<h1>Hello World!11!</h1>")
 })
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`)
